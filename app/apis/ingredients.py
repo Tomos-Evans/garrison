5d421f7d3979db0a59399785f22ae9a9d0a08d25
Tom Evans <TomosAMEvans@gmail.com>
2018-10-02 20:41:31 +0100

    TE10 Ingredient API (#3)
    * adds route for single ingredient
    
    * get single ingredient
    
    * adds post ingredients
    
    * added ingredient api code docs
    
    * tested ingredient post

--- app/apis/ingredients.py
+++ app/apis/ingredients.py
@@ -1,10 +1,15 @@
 import json
-from flask_restplus import Resource, Namespace
-from flask import current_app
+from flask_restplus import Resource, Namespace, abort, reqparse, fields, inputs
+from flask import current_app, jsonify, make_response, url_for
 
 from app.models.drinks import Ingredient
 
 ns = Namespace('ingredients', description="The ingredients that the garrison has.")
+
+post_parser = reqparse.RequestParser()
+post_parser.add_argument('name', type=str, help='The name of the ingredient', required=True)
+post_parser.add_argument('alcoholic', type=inputs.boolean, help='Is the ingredient alcoholic', required=True)
+post_parser.add_argument('abs', type=int, help='The ABS of the ingredient if alcoholic', required=False)
 
 @ns.route('/')
 class Ingredients(Resource):
@@ -13,5 +18,30 @@ class Ingredients(Resource):
             'ingredients': list(map(lambda i: i.as_json(), Ingredient.query.all()))
         }
 
+    @ns.doc(parser=post_parser)
+    @ns.response(201, 'Created')
+    @ns.response(400, 'Alcoholic drinks must specify ABS')
+    @ns.response(409, 'Name conflict')
     def post(self):
-        pass
+        args = post_parser.parse_args()
+        name = args['name']
+        alcoholic = args['alcoholic']
+        abs = args['abs']
+
+        if alcoholic and not abs:
+            abort(400, "alcoholic drinks must specify ABS")
+        if Ingredient.query.filter_by(name=name).first():
+            abort(409, "Name already in use")
+        i = Ingredient.from_params(name, alcoholic, abs)
+        return make_response(jsonify(href=url_for('ingredients_ingredients') + i.ref), 201)
+
+@ns.route('/<ref>')
+class SingleIngredient(Resource):
+    @ns.response(200, 'Ok')
+    @ns.response(404, 'Not found')
+    def get(self, ref):
+        i = Ingredient.query.filter_by(ref=ref).first()
+        if i:
+            return i.as_json()
+        else:
+            abort(404, "No ingredient with ref: " + ref)
--- test/apis/ingredients.py
+++ test/apis/ingredients.py
@@ -33,3 +33,50 @@ class TestGet(ApiTestCase):
             ]
         }
         self.assertEqual(response.json, target)
+
+    def test_get_one_that_exists(self):
+        ref = Ingredient.query.all()[0].ref
+        response = self.client.get('/api/ingredients/'+ref)
+        target = {
+            'href': '/api/ingredients/'+ref,
+            'name': 'vodka',
+            'alcoholic': True,
+            'abs': 40,
+        }
+        self.assertEqual(response.json, target)
+
+    def test_one_doesnt_exist(self):
+        response = self.client.get('/api/ingredients/not-a-ref')
+        self.assertEqual(response.status_code, 404)
+
+    def test_ing_post(self):
+        a = {
+            'name': 'new ing',
+            'alcoholic': True,
+            'abs': 12
+        }
+        b = {
+            'name': 'new ing2',
+            'alcoholic': False
+        }
+        response = self.client.post('/api/ingredients/', json=a)
+        self.assertEqual(response.status_code, 201)
+        response = self.client.post('/api/ingredients/', json=b)
+        self.assertEqual(response.status_code, 201)
+
+    def test_abs_requirement(self):
+        a = {
+            'name': 'new ing',
+            'alcoholic': True
+        }
+        response = self.client.post('/api/ingredients/', json=a)
+        self.assertEqual(response.status_code, 400)
+
+    def test_ing_name_confilct(self):
+        a = {
+            'name': 'vodka',
+            'alcoholic': True,
+            'abs': 1
+        }
+        response = self.client.post('/api/ingredients/', json=a)
+        self.assertEqual(response.status_code, 409)
