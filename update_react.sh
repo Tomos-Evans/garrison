2ac1ad6f63c59d3eb7a0536f1cee39cc43e07c29
Tomos Evans <tomosamevans@gmail.com>
2019-03-09 21:26:30 +0000

    ract

--- update_react.sh
+++ update_react.sh
@@ -3,15 +3,7 @@
 rm -rf app/templates
 rm -rf app/static
 
-git clone https://github.com/beepboop-tech/grace-material.git react && cd react && cd grace
-
-npm install
-npm run build
-
-# cd react
-# cd grace
-
-cd ..
+git clone https://github.com/beepboop-tech/grace-material.git react && cd react
 
 mkdir -p ../app/templates
 mkdir -p ../app/static
